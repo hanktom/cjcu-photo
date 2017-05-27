f478953cdb21fe2c2a4b23284591e546cd6a3ca6
Hank Tom <hanktom@gmail.com>
2017-05-27 15:55:26 +0800

    get selected file path

--- app/src/main/java/com/tom/photo/PhotoActivity.java
+++ app/src/main/java/com/tom/photo/PhotoActivity.java
@@ -4,20 +4,38 @@ import android.*;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class PhotoActivity extends AppCompatActivity {
 
     private static final int REQUEST_PICK_PHOTO = 900;
+    private static final String TAG = PhotoActivity.class.getSimpleName();
+    private String selectedImagePath;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo);
+        image = (ImageView) findViewById(R.id.image);
     }
 
     public void pick(View view){
@@ -46,7 +64,52 @@ public class PhotoActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_PICK_PHOTO){
-            
+            Uri originUri = data.getData();
+            try {
+                Bitmap bitmap =
+                        MediaStore.Images.Media.getBitmap(getContentResolver(), originUri);
+                final int takeFlags = data.getFlags() &
+                        (Intent.FLAG_GRANT_READ_URI_PERMISSION |
+                                Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                //noinspection WrongConstant
+                getContentResolver().takePersistableUriPermission(originUri, takeFlags);
+                String id = originUri.getLastPathSegment().split(":")[1];
+                final String[] imagesColumns = {MediaStore.Images.Media.DATA};
+                final String orderBy = null;
+                Uri uri = getUri();
+                selectedImagePath = null;
+                Cursor cusror = managedQuery(uri, imagesColumns,
+                        MediaStore.Images.Media._ID +"=" +id, null, orderBy);
+                if (cusror.moveToFirst()){
+                    selectedImagePath =
+                            cusror.getString(cusror.getColumnIndex(MediaStore.Images.Media.DATA));
+                }
+                Log.d(TAG, selectedImagePath);
+                image.setImageBitmap(bitmap);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
+
+    }
+    private Uri getUri(){
+        String state = Environment.getExternalStorageState();
+        if (!state.equalsIgnoreCase(Environment.MEDIA_MOUNTED)){
+            return MediaStore.Images.Media.INTERNAL_CONTENT_URI;
+        }
+
+        return MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+    }
+
+    public void upload(View view){
+        StorageReference storage = FirebaseStorage.getInstance().getReference("aa");
+        try {
+            FileInputStream fis = new FileInputStream(selectedImagePath);
+            storage.putStream(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 }
